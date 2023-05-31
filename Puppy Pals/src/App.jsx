bb1448dd79d492693010f51e3c59ad8bd5de2427
Agustin Zamora Jr <agustinzamorajr@Agustins-MacBook-Pro.local>
2023-05-31 17:00:50 -0500

    second commit

--- Puppy Pals/src/App.jsx
+++ Puppy Pals/src/App.jsx
@@ -1,10 +1,32 @@
-import { useState } from 'react'
+import {puppyList} from './data.js'
+import {puppyList} from './data.js'
 import './App.css'
 
 function App() {
+  const [puppies, setPuppies] = useState(puppyList)
+  const [featPupId, setFeatPupId] = useState(null)
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
 
+  return (
+    <div className="App">
+      {
+        puppies.map((puppy) => {
+          return <button className="PuppyButton" onClick={()=>{setFeatPupId
+          (puppy.id)}} key={puppy.id}>{puppy.name}</button>
+        })
+      }
+    {featPupId && (
+      <div className="puppyId">
+        <h2>{featuredPup.name}</h2>
+        <ul>
+          <li className="puppyDetails">Age: {featuredPup.age}</li>
+          <li className="puppyDetails">email: {featuredPup.email}</li>
+        </ul>
+      </div>
+    )}
+    </div>
 
-  return "Hello World"
+  )
 }
 
 export default App
